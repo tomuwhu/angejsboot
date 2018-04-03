df90d2cd4044d1bb7355bc33b0b69c5781823b6b
tomuwhu <34095687+tomuwhu@users.noreply.github.com>
2018-04-03 09:29:03 +0200

    new

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
-})
+router.get( '/', (req, res, next) => res.render('index',{ iv: 123 }) )
 
 module.exports = router
--- routes/req.js
+++ routes/req.js
@@ -2,18 +2,14 @@ var express = require('express')
 var mongoose = require('mongoose')
 var router = express.Router()
 
-router.get('/', (req, res, next) =>
-  res.send('Root query')
-)
+router.get('/', (req, res, next) => res.send('Root query'))
 
 mongoose.connect('mongodb://localhost/spny')
 var Csapat = mongoose.model( 'Csapat', {} )
-router.get('/csapatok', (req, res, next) => {
+router.get( '/csapatok', (req, res, next) =>
   Csapat
-      .find()
-      .then( found => {
-        res.send( found )
-      } )
-})
+      .find( )
+      .then( found => res.send( found ) )
+)
 
 module.exports = router
