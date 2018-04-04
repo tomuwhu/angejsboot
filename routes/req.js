a114e1b21c8ad58a54c79f9c54eb82190eb0057d
Németh Tamás <tomuwhu@gmail.com>
2018-04-04 19:15:59 +0200

    bs4

--- routes/req.js
+++ routes/req.js
@@ -2,6 +2,8 @@ var express  = require('express')
 var mongoose = require('mongoose')
 var router   = express.Router()
 
+//      ->  /req/
+
 router.get('/', (req, res, next) => res.send('Root query'))
 
 module.exports = router
