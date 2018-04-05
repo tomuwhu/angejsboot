9031eef88db5ca4119b138ffda9341de74147fef
Tamás Németh <tomuwhu@gmail.com>
2018-04-05 18:53:12 +0200

    kész

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ var router = express.Router()
 
 router.get( '/', (req, res, next) => res.render('index') )
 
+router.get( '/menu', (req, res, next) => res.render('menu') )
+
 module.exports = router
