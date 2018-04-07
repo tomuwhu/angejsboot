f6fa46242a7b8b002f59ed9e446204ba3c8a6526
Tamás Németh <tomuwhu@gmail.com>
2018-04-07 17:51:34 +0200

    err

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
   res.status(err.status || 500)
-  res.render('error')
+  res.render('system/error')
 })
 module.exports = app
