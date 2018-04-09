9caf6ddcf5828749a0fd63c673d6b1513f8b5d1b
Tamás Németh <tomuwhu@gmail.com>
2018-04-09 19:58:17 +0200

    auto_menus

--- router.js
+++ router.js
@@ -1,7 +1,17 @@
 var express = require('express')
 var router = express.Router()
 
-router.get( '/', (req, res, next) =>
-       res.render('index', { am: 'gy' } ) )
+menu = [
+  { url: '/'    , felirat: 'Kezdőlap'      },
+  { url: 'ures' , felirat: 'Üres menüpont' }
+]
+
+menu.forEach( (v,k) => {
+  if (v.url=='/') v.ejs_file='index'
+  else v.ejs_file=v.url
+  router.get( "/"+v.url, (req, res) => {
+         res.render( v.ejs_file, { menu: menu, index:k } )
+  } )
+} )
 
 module.exports = router
