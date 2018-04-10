839c3318db36a19270ebdf032f90fd0768294cdb
Tamás Németh <tomuwhu@gmail.com>
2018-04-10 20:34:50 +0200

    .

--- router.js
+++ router.js
@@ -1,15 +1,14 @@
 var express = require('express')
 var router = express.Router()
-
+//const mongoose = require('mongoose')
+//mongoose.connect('mongodb://localhost/animals')
 menu = [
   { url: '/'    , felirat: 'Kezdőlap'      },
   { url: 'ures' , felirat: 'Üres menüpont' }
 ]
-
 menu.forEach( (v,k) => {
   v.url == '/' ? v.ejs_file = 'index' : v.ejs_file = v.url
   router.get( '/' + v.url, (req, res) =>
         res.render( v.ejs_file, { menu: menu, index:k } ) )
 } )
-
 module.exports = router
